a76d00648c06e9c49bf6c0e4ddc8a48915c8fc6d
Nikolas <etc.nikolas@gmail.com>
2024-08-11 11:36:37 +0200

    Refactor form variant creation

---
+++ formVariantRef.lua
@@ -0,0 +1,36 @@
+---@class formVariant
+local formVariant = {}
+formVariant.__index = formVariant
+
+formVariant.iconImage = nil
+formVariant.hintImage = nil
+formVariant.connectionLimit = nil
+formVariant.reach = nil
+formVariant.bodyRadius = nil
+formVariant.imgScale = 1
+formVariant.shiftX = 0
+formVariant.shiftY = 0
+formVariant.score = function() return 0 end
+
+function formVariant:setImageParameters(scale, shiftX, shiftY)
+    self.imgScale = scale
+    self.shiftX = shiftX
+    self.shiftY = shiftY
+end
+
+function formVariant:new(connectionLimit, reach, bodyRadius, iconImagePath, hintImagePath)
+    local new = setmetatable({},self)
+    new.connectionLimit = connectionLimit
+    new.reach = reach
+    new.bodyRadius = bodyRadius
+    new.iconImage = love.graphics.newImage(iconImagePath)
+    new.hintImage = love.graphics.newImage(hintImagePath)
+    return new
+end
+
+setmetatable(formVariant,
+{__call = function()
+    return formVariant:new()
+end})
+
+return formVariant
