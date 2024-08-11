f8f0e38c459b4c729cf336a5df9b46b90bc9c60a
Nikolas <etc.nikolas@gmail.com>
2024-08-11 21:03:27 +0200

    Create random variants

--- formVariants/formVariantFactory.lua
+++ formVariants/formVariantFactory.lua
@@ -26,3 +26,33 @@ setmetatable(formVariantFactory.created,
     return self[_k]
 end})
 
+function formVariantFactory.getRandomVariants(pools,amount)
+    local variants = {}
+    local indexCounter = 1
+    local amountOfVariants = 0
+    --Count all available variants in given pools
+    for _,pool in ipairs(pools) do
+	for _,_ in pairs(FORM_VARIANT_POOL_NAMES[pool]) do
+	    amountOfVariants = amountOfVariants + 1
+	end
+    end
+
+    --Get random variant amount times
+    for cur = 1, amount do
+	indexCounter = 1
+	local randomIndex = love.math.random(amountOfVariants)
+	for _,pool in ipairs(pools) do
+	    for _,variant in pairs(FORM_VARIANT_POOL_NAMES[pool]) do
+		if indexCounter == randomIndex then
+		    --This will either get created variant 
+		    --or create it per metamethod
+		    variants[cur] = formVariantFactory.created[variant]
+		end
+		indexCounter = indexCounter + 1
+	    end
+	end
+    end
+    return variants
+end
+
+return formVariantFactory
